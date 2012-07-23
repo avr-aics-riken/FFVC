dab8085ccc5932cc0c4a8d9b7f05dfebec3acf3e
Kenji Ono <keno@riken.jp>
2012-07-23 16:04:50 +0900

    r689 FB92/FFV50

---
+++ example/PMT/omp/change.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mv condition.txt condition_$1.txt
+mv DomainInfo.txt DomainInfo_$1.txt
+mv history_base.txt history_base_$1.txt
+mv history_domainflux.txt history_domainflux_$1.txt
+mv profiling.txt profiling_$1.txt
---
+++ example/PMT/omp/go.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+a=1
+while [ $a -le 12 ]
+do
+export OMP_NUM_THREADS=$a
+ffvc pm_1.tp domain.tp
+./change.sh $a
+a=`expr $a + 1`
+done
