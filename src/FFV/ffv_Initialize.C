a601d7a2b9be7b5b9061ce9f0fdef540fb80f5d8
Kenji Ono <keno@riken.jp>
2013-06-08 12:38:56 +0900

    r967 Modify division policy again

--- src/FFV/ffv_Initialize.C
+++ src/FFV/ffv_Initialize.C
@@ -2273,14 +2273,7 @@ int FFV::get_DomainInfo(TPControl* tp_dom)
     Hostonly_ cout << "\tNeither GlobalVoxel nor GlobalPitch is specified. You need to choose either parameter.\n" << endl;
     Exit(0);
   }
-  /* >> 20130606 コメントアウト
-  else if ( g_flag && p_flag ) // 両方が指定されている
-  {
-    Hostonly_ cout << "\tBoth GlobalVoxel and GlobalPitch is specified. You need to choose either parameter." << endl;
-    Exit(0);
-  }
-   */
-  else if ( g_flag ) // G_voxelの指定がある >> G_voxelが優先　&& !p_flag
+  else if ( g_flag ) // G_voxelの指定がある >> G_voxelが優先
   {
     if ( g_flag )
     {
@@ -2321,7 +2314,7 @@ int FFV::get_DomainInfo(TPControl* tp_dom)
       }
     }
   }
-  else if ( !g_flag && p_flag ) // pitchのみ >> @todo 機能するかチェックすること 20130606
+  else if ( !g_flag && p_flag ) // pitchのみ
   {
     if ( (pitch[0]>0.0) && (pitch[1]>0.0) && (pitch[2]>0.0) )
     {
