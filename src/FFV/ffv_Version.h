77d06cb20b5ab2ae932897b08580ad105b698c23
Kenji Ono <keno@riken.jp>
2013-11-02 08:04:50 +0900

    r1046 compile on K/FX [ver 1.4.9]

--- src/FFV/ffv_Version.h
+++ src/FFV/ffv_Version.h
@@ -20,8 +20,8 @@
 #ifndef _FFVC_VERSION_H_
 #define _FFVC_VERSION_H_
 
-#define FFVC_VERSION_NO  "1.4.8"
-#define FFVC_REVISION    "20131030_1830"
+#define FFVC_VERSION_NO  "1.4.9"
+#define FFVC_REVISION    "20131102_0030"
 
 #endif /* _FFVC_VERSION_H_ */
 
