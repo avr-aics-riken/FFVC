b7027e95dfe09677a3900f155147ab36fa099848
Kenji Ono <keno@riken.jp>
2015-09-13 14:27:46 +0900

    r1636 suppress check print

--- src/FFV/ffv_Version.h
+++ src/FFV/ffv_Version.h
@@ -20,8 +20,8 @@
 #ifndef _FFVC_VERSION_H_
 #define _FFVC_VERSION_H_
 
-#define FFVC_VERSION_NO  "2.4.0"
-#define FFVC_REVISION    "20150912_1500"
+#define FFVC_VERSION_NO  "2.4.1"
+#define FFVC_REVISION    "20150913_1500"
 
 #endif /* _FFVC_VERSION_H_ */
 
